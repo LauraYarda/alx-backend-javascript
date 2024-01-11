5e3e495461ed8a3b7a1ccd7ecae00577b47ec3a7
laurayarda <laurayarda5@gmail.com>
2024-01-11 16:14:02 +0000

    Files

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -12,30 +12,30 @@ interface TeacherInterface {
 
 class Director implements DirectorInterface {
     workFromHome(): string {
-        return Working from home
+        return "Working from home"
     }
     getCoffeeBreak(): string {
-        return Getting a coffee break
+        return "Getting a coffee break"
     }
     workDirectorTasks(): string {
-        return Getting to director tasks
+        return "Getting to director tasks"
     }
 }
 
 class Teacher implements TeacherInterface {
     workFromHome(): string {
-        return Cannot work from home
+        return "Cannot work from home"
     }
     getCoffeeBreak(): string {
-        return Cannot have a break
+        return "Cannot have a break"
     }
     workTeacherTasks(): string {
-        return Getting to work
+        return "Getting to work"
     }
 }
 
 function createEmployee(salary: number | string) {
-    if (typeof salary === number && salary < 500) {
+    if (typeof salary === "number" && salary < 500) {
         return Teacher
     }
     return Director
